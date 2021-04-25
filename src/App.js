c74f36447903ccbc45b9a2aef985b1dc9ef5d81f
Yehia Khalil <yehiakhalilykk@gamil.com>
2021-04-25 20:06:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employee: {
+        "id": "1",
+        "jobTitleName": "Developer",
+        "firstName": "Ahmed",
+        "lastName": "Ali",
+        "preferredFullName": "Ahmed Ali",
+        "employeeCode": "E1",
+        "region": "CA",
+        "phoneNumber": "408-1234567",
+        "emailAddress": "ahmed.ali@gmail.com"
+      }
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Obj Employee= {this.state.employee} />
+      </div>
+    );
+  }
+}
+
+
+class Obj extends React.Component {
+  constructor(props){
+    super();
+    this.state={
+      obj:props.Employee
+    }
+  }
+  render() {
+    return (
+      <div>
+        Id: {this.state.obj.id} <br/>
+        Name: {this.state.obj.preferredFullName}<br/>
+        Job: {this.state.obj.jobTitleName}<br/>
+        Region: {this.state.obj.region}<br/>
+        Number: {this.state.obj.phoneNumber}
+
+      </div>
+      );
+  }
 }
 
 export default App;
